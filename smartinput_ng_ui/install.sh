1bd0c00f2180b9ef4cc62b6b0d227d69845f022c
niwla23 <alwin@cloudserver.click>
2022-05-26 15:46:55 +0200

    let install.sh download the ui

--- smartinput_ng_ui/install.sh
+++ smartinput_ng_ui/install.sh
@@ -1,1 +1,3 @@
 pip3 install smartinput-ng-daemon
+sudo wget -O /usr/local/bin/smartinput_ng_ui https://github.com/niwla23/smartinput_ng/releases/download/ui-v0.1.0/smartinput-ng-ui
+sudo chmod a+x /usr/local/bin/smartinput_ng_ui
