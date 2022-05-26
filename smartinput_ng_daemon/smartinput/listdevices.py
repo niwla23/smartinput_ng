cd1164651c35a4c0986881d9488c264e6d1aeb43
niwla23 <alwin@cloudserver.click>
2022-05-26 21:24:49 +0200

    update installer

--- smartinput_ng_daemon/smartinput/listdevices.py
+++ smartinput_ng_daemon/smartinput/listdevices.py
@@ -1,4 +1,10 @@
 from serial.tools.list_ports import comports
 
-for port in comports():
-    print(f"{port.name}: {port.hwid}")
+
+def main():
+    for port in comports():
+        print(f"{port.name}: {port.hwid}")
+
+
+if __name__ == "__main__":
+    main()
